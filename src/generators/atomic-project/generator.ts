0ccd1043d3f933baf340788436af601f5690d7db
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-13 13:49:32 -0400

    Starting template nx generator configuration

---
+++ src/generators/atomic-project/generator.ts
@@ -0,0 +1,76 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  names,
+  offsetFromRoot,
+  Tree,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import { AtomicProjectGeneratorSchema } from './schema';
+
+interface NormalizedSchema extends AtomicProjectGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  organizationName?: string;
+  parsedTags: string[];
+}
+
+function normalizeOptions(tree: Tree, options: AtomicProjectGeneratorSchema): NormalizedSchema {
+  let name = names(options.projectName).fileName
+  let organizationName;
+  if (name.includes("@") && name.split('/').length > 1) {
+    organizationName = options.projectName.split('/')[0];
+  }
+  let projectName = !!organizationName ? options.projectName.split('/')[1] : name;
+  projectName = projectName.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = !!organizationName ? `${getWorkspaceLayout(tree).appsDir}/${organizationName}/${projectName}` : `${getWorkspaceLayout(tree).libsDir}/${projectName}`;
+  const parsedTags = options.tags
+    ? options.tags.split(',').map((s) => s.trim())
+    : [];
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+    organizationName,
+    parsedTags,
+  };
+}
+
+function addFiles(tree: Tree, options: NormalizedSchema) {
+    const templateOptions = {
+      ...options,
+      ...names(options.projectName),
+      offsetFromRoot: offsetFromRoot(options.projectRoot),
+      template: '',
+
+      packageName: !!options.organizationName ? `${options.organizationName}/${options.projectName}` : options.projectName,
+      moduleName: options.projectName
+    };
+
+    // Generate our base project files
+    generateFiles(tree, path.join(__dirname, '../base-files'), options.projectRoot, templateOptions);
+
+    // Generate our project specific files
+    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
+}
+
+export default async function (tree: Tree, options: AtomicProjectGeneratorSchema) {
+  const normalizedOptions = normalizeOptions(tree, options);
+  addProjectConfiguration(
+    tree,
+    normalizedOptions.projectName,
+    {
+      root: normalizedOptions.projectRoot,
+      projectType: 'application',
+      sourceRoot: `${normalizedOptions.projectRoot}/src`,
+      targets: {
+      },
+      tags: normalizedOptions.parsedTags,
+    }
+  );
+  addFiles(tree, normalizedOptions);
+  await formatFiles(tree);
+}
